49252339d9183afbb58905db0ab2c36a71b8a6dc
AshleeGarcia <ashleegarcia.dev@gmail.com>
2025-11-14 21:50:48 -0500

    Refactor App component to integrate notification handling and replace default view with HomeScreen

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import * as Notifications from 'expo-notifications';
+import HomeScreen from './src/screens/HomeScreen';
+import registerForNotificationsAsync from './src/notifications/register';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+    shouldShowBanner: true,
+    shouldShowList: true,
+  }),
+});
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  useEffect(() => {
+    registerForNotificationsAsync();
+  }, []);
+
+  return <HomeScreen />;
+}
